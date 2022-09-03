558af90d3ac0cecf531b412345c0afcbf94f2c18
Virag Kormoczy <vkormoczy@gmail.com>
2022-09-03 15:11:14 +0300

    Add fibonacci solutions

--- Recursion/fibonacci.js
+++ Recursion/fibonacci.js
@@ -10,3 +10,37 @@ const sumFibonacci = (num) => {
 };
 
 console.log(sumFibonacci(7)); // 1,1,2,3,5,8,13 => 13
+
+const fib = (num) => {
+  let a = 0;
+  let b = 1;
+  let arr = [];
+
+  arr.push(a, b);
+
+  for (let i = 0; i < num - 2; i++) {
+    let c = a + b;
+    a = b;
+    b = c;
+
+    arr.push(c);
+  }
+  return arr;
+};
+
+//console.log(fib(8)); // [0,1,1,2,3,5,8,13]
+
+const fibRecursive = (n) => {
+  if (n < 2) {
+    return [1];
+  }
+  if (n < 3) {
+    return [1, 1];
+  }
+
+  let a = fib(n - 1);
+  a.push(a[n - 2] + a[n - 3]);
+  return a;
+};
+
+console.log(fibRecursive(8)); // [0,1,1,2,3,5,8,13]
